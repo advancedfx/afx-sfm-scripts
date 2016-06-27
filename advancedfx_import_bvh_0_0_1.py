0b5a55b6b67e15e2d1b9d1487bbcfb2afbbc2e76
Dominik Tugend <dominik@matrixstorm.com>
2016-06-27 21:58:56 +0200

    Woops.

--- advancedfx_import_bvh_0_0_1.py
+++ advancedfx_import_bvh_0_0_1.py
@@ -1,22 +1,3 @@
-# How to use:
-#
-# Installation:
-# Place this file in the "SourceFilmmaker\game\platform\scripts\sfm\animset"
-# folder.
-#
-# Usage:
-# 1. Edit a clip
-# 2. Add a camera: Right click in empty space on Animation Set Editor and select
-#    "Create Animation Set for New Camera"
-# 3. Right click the camera and select Rig advancedfx_import_bvh_0_0_1
-# The import will not set the correct FOV for the camera (the BVH file also does
-# not contain FOV information), that is left up to the
-# pros atm (maybe I will make a feature for that in the future).
-# Also be aware that CS:GO actually uses an higher FOV than you set:
-#
-# advancedfx_export_bvh is still in the making (sorry).
-
-
 # Copyright (c) advancedfx.org
 #
 # Last changes:
